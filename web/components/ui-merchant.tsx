cbd70de025d8a82d02879adddd6cc722c3e8b752
mattiasadem <168101829+mattiasadem@users.noreply.github.com>
2026-09-04 10:02:50 +0200

    fix: restore ui-merchant as barrel re-exports

--- web/components/ui-merchant.tsx
+++ web/components/ui-merchant.tsx
@@ -1,1 +1,6 @@
-PLACEHOLDER
+"use client";
+export { MetricCards, MiniBars, AlertList } from "@/components/ui-merchant-metrics";
+export { MerchantChat } from "@/components/ui-merchant-chat";
+export { StagedQueue } from "@/components/ui-merchant-staged";
+export { CatalogTable, StockView } from "@/components/ui-merchant-catalog";
+export { OrdersView } from "@/components/ui-merchant-orders";
