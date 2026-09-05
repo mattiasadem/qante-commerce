2195394c42154ed37a75feab18619e4f27037fc3
mattiasadem <168101829+mattiasadem@users.noreply.github.com>
2026-09-05 13:41:18 +0200

    fix(shop): restore thin ui-shop.tsx re-exports (core/home/pdp)

--- web/components/ui-shop.tsx
+++ web/components/ui-shop.tsx
@@ -1,1 +1,15 @@
-USE_FILE:/tmp/ui_shop_content_only.txt
+"use client";
+export {
+  AddButton,
+  BuyNowButton,
+  ProductCard,
+  ProductGrid,
+} from "@/components/ui-shop-core";
+export { HomeView } from "@/components/ui-shop-home";
+export {
+  AskAboutProduct,
+  PdpView,
+  CartPageView,
+  OrderConfirm,
+} from "@/components/ui-shop-pdp";
+export { AssistantPane, AssistantRail, AssistantSheet } from "@/components/GenAssistant";
