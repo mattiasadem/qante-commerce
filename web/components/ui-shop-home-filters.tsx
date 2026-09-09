7d661d4ab406a017935c1522f4156060ebfa1213
mattiasadem <168101829+mattiasadem@users.noreply.github.com>
2026-09-09 21:33:07 +0200

    fix(home): wrap filters so setInStockOnly accepts updaters

--- web/components/ui-shop-home-filters.tsx
+++ web/components/ui-shop-home-filters.tsx
@@ -1,2 +1,25 @@
 "use client";
-export { parseSort, buildHomeQs, useHomeFilters } from "./ui-shop-home-filters-impl";
+import {
+  parseSort,
+  buildHomeQs,
+  useHomeFilters as useHomeFiltersImpl,
+} from "./ui-shop-home-filters-impl";
+
+export { parseSort, buildHomeQs };
+
+type FilterOpts = Parameters<typeof useHomeFiltersImpl>[0];
+
+export function useHomeFilters(opts: FilterOpts) {
+  const h = useHomeFiltersImpl(opts);
+  return {
+    ...h,
+    setInStockOnly: (next: boolean | ((v: boolean) => boolean)) => {
+      const value = typeof next === "function" ? next(h.inStockOnly) : next;
+      h.setInStockOnly(value);
+    },
+    setOutOfStockOnly: (next: boolean | ((v: boolean) => boolean)) => {
+      const value = typeof next === "function" ? next(h.outOfStockOnly) : next;
+      h.setOutOfStockOnly(value);
+    },
+  };
+}
